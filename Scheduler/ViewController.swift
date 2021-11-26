741d5e26d9e6c9e3412ec8f03775a01c3a113fde
ob1vansk1y <obivanskiy@gmail.com>
2021-11-26 14:36:24 +0200

    Initial Commit

---
+++ Scheduler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scheduler
+//
+//  Created by Иван Ободовский on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
